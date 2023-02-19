5d078af37ef7894f8a45ccd5bf7fb2436f3be3bf
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2023-02-19 17:28:17 +0300

    gradle updates

--- app/src/main/java/com/atilsamancioglu/retrofitresponsehandling/view/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/retrofitresponsehandling/view/MainActivity.kt
@@ -7,16 +7,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.atilsamancioglu.retrofitresponsehandling.R
 import com.atilsamancioglu.retrofitresponsehandling.adapter.RecyclerViewAdapter
+import com.atilsamancioglu.retrofitresponsehandling.databinding.ActivityMainBinding
 import com.atilsamancioglu.retrofitresponsehandling.model.CryptoModel
 import com.atilsamancioglu.retrofitresponsehandling.service.CryptoAPI
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
+    private lateinit var binding: ActivityMainBinding
     private val BASE_URL = "https://raw.githubusercontent.com/"
     private var cryptoModels: ArrayList<CryptoModel>? = null
     private var recyclerViewAdapter : RecyclerViewAdapter? = null
@@ -33,7 +34,10 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
 
         //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
         //https://api.nomics.com/v1/prices?key=2187154b76945f2373394aa34f7dc98a
@@ -44,7 +48,7 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
         //RecyclerView
 
         val layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+        binding.recyclerView.layoutManager = layoutManager
 
         loadData()
 
@@ -55,20 +59,20 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
         val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                //.addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build().create(CryptoAPI::class.java)
 
 
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
             val response = retrofit.getData()
-
+            println(response.body())
             withContext(Dispatchers.Main){
                 if(response.isSuccessful) {
                     response.body()?.let {
                         cryptoModels = ArrayList(it)
                         cryptoModels?.let {
                             recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
-                            recyclerView.adapter = recyclerViewAdapter
+                            binding.recyclerView.adapter = recyclerViewAdapter
                         }
                     }
                 }
@@ -106,7 +110,7 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
 
                             cryptoModels?.let {
                                 recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
-                                recyclerView.adapter = recyclerViewAdapter
+                                binding.recyclerView.adapter = recyclerViewAdapter
                             }
 
 
@@ -135,7 +139,7 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
 
         cryptoModels?.let {
             recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
-            recyclerView.adapter = recyclerViewAdapter
+            binding.recyclerView.adapter = recyclerViewAdapter
         }
     }
 
